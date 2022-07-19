32137b5aec22f59b6590cdb9318f930e91e11cb6
Brener Rosa <brenersrosa@gmail.com>
2022-07-19 17:25:54 -0300

    :fire: home screen

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import {
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
 import { THEME } from "./src/styles/theme";
-import { SignIn } from "./src/screens/SignIn";
+import { Home } from "./src/screens/Home";
 import { Loading } from "./src/components/Loading";
 
 export default function App() {
@@ -18,7 +18,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <SignIn /> : <Loading />}
+      {fontsLoaded ? <Home /> : <Loading />}
     </NativeBaseProvider>
   );
 }
