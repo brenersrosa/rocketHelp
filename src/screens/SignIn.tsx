7477aee1c773ced75e6cf9d3f96cfda3e2ab903a
Brener Rosa <brenersrosa@gmail.com>
2022-07-21 09:15:44 -0300

    :fire: setting Google auth

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,18 +1,34 @@
-import { Heading, Icon, VStack, useTheme } from "native-base";
-import { Envelope, Key } from "phosphor-react-native";
-import { useState } from "react";
+import { Heading, Icon, VStack, useTheme } from 'native-base';
+import auth from '@react-native-firebase/auth';
+import { Alert } from 'react-native';
+import { Envelope, Key } from 'phosphor-react-native';
+import { useState } from 'react';
 
-import Logo from "../assets/logo_primary.svg";
-import { Button } from "../components/Button";
-import { Input } from "../components/Input";
+import Logo from '../assets/logo_primary.svg';
+import { Button } from '../components/Button';
+import { Input } from '../components/Input';
 
 export function SignIn() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   const { colors } = useTheme();
 
-  function handleSignIn() {}
+  function handleSignIn() {
+    if (!email || !password) {
+      return Alert.alert('Entrar', 'Informe e-mail e senha.');
+    }
+
+    setIsLoading(true);
+
+    auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch((error) => {
+        console.log(error);
+        setIsLoading(false);
+      });
+  }
 
   return (
     <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
@@ -27,7 +43,7 @@ export function SignIn() {
           <Icon as={<Envelope color={colors.gray[300]} />} ml={4} />
         }
         value={email}
-        onChangeText={(e) => setEmail(e)}
+        onChangeText={setEmail}
       />
       <Input
         mb={8}
@@ -36,7 +52,7 @@ export function SignIn() {
         secureTextEntry
         type="password"
         value={password}
-        onChangeText={(e) => setPassword(e)}
+        onChangeText={setPassword}
       />
       <Button title="Entrar" onPress={handleSignIn} />
     </VStack>
